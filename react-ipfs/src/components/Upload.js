100cbe9ce73c3fca333e2ad7147550d65888aa23
cccrizzz <ccccrizzzz@gmail.com>
2021-12-20 06:01:04 -0500

    tried to fix

--- react-ipfs/src/components/Upload.js
+++ react-ipfs/src/components/Upload.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import { create, urlSource } from 'ipfs-http-client'
+import { create } from 'ipfs-http-client'
 
 // create ipfs http-client
-const client = create("http://localhost:5001")
-
-
+// const client = create("http://localhost:5001")
+const client = create("https://ipfs.infura.io:5001/api/v0")
+console.log(client)
 
 export default class Upload extends Component {
 
@@ -18,21 +18,33 @@ export default class Upload extends Component {
     }
 
     // update target file
-    onFileChange = (event) => {
+    onFileChange =  (event) => {
         this.setState({ selectedFile: event.target.files[0]})
+
     }
 
     // upload to IPFS
-    upload = async () => {
-        // console.log(await client.version())
+    upload = async (event) => {
+        console.log(this.state.selectedFile)
 
-        // for await (const result of client.add(this.state.selectedFile)) {               
-        //     console.log(result)
-        //     this.setState({hash: result.path})
+        console.log("upload pressed")
+        console.log(await client.version())
+        
+        // const reader = new window.FileReader()
+        // reader.readAsArrayBuffer(this.state.selectedFile)
+        // reader.onloaded = async () => {
+        //     const fileBuffer = Buffer.from(reader.result)
+        //     const hash = await client.add(fileBuffer, (err, fileHash) => {
+        //         return fileHash
+        //     })
+        //     console.log(hash)
         // }
-    
-        let hash = await client.add(this.state.selectedFile)
-        console.log(hash)
+
+
+        // await client.add(this.state.selectedFile, (err, fileHash) => {
+        //     this.setState({hash: fileHash})
+        //     console.log(fileHash)
+        // })
     }
 
     render() {
